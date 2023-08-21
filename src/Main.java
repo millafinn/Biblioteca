75a7782e1034c6a6a637e66e8a1846ab3500ffcc
camila.guimaraes <milla_finn@hotmail.com>
2023-08-21 12:21:04 -0300

    oi

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,21 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner inp = new Scanner(System.in);
+        Livro book[] = new Livro[999];
+       String retorno = inp.next();
+
+
+        do{
+            System.out.println("Digite 1, para cadastrar um livro: ");
+            System.out.println("Digite 2, procurar livro: ");
+            System.out.println("Digite 3, listar um livro: ");
+            System.out.println("Digite 4, quantidade cadastrada de livro: ");
+            System.out.println("Digite 5, excluir ultimo livro da lista: ");
+            System.out.println("Digite 6, ordenar livros cadastrados: ");
+            System.out.println("Digite 7, encerrar programa");
+            retorno = inp.next();
+
+
+        }while (!retorno.equals("7"));
     }
 }
